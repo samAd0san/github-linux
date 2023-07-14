102b4f41445f7a0c931554170b8af5aafbffb651
samad <sohaibsamad.hala@gmail.com>
2023-07-14 02:28:54 +0000

    bug resolved in master branch

--- savior.java
+++ savior.java
@@ -1,6 +1,6 @@
 public class savior {
 	public static void main(String[] args) {
-		System.out.println("BUG RESOLVED");
+		System.out.println("BUG RESOLVED IN MASTER BRANCH");
 		System.out.println("Execution successfull");
 	}
 }
